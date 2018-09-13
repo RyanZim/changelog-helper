c9e97b284fd91276c43b8b194f51dbec61da0cc7
Ryan Zimmerman <opensrc@ryanzim.com>
2018-09-12 21:53:34 -0400

    Use existing header style when prepending to files

--- index.js
+++ index.js
@@ -7,14 +7,29 @@ const opn = require('opn');
 const timestamp = require('time-stamp');
 const startServer = require('./server');
 const githubNumberToLink = require('./lib/github-number-to-link');
+const textToHeading = require('./lib/text-to-heading');
 
 const { pkg, path: pkgPath } = readPkgUp.sync();
-const github = githubUrlFromGit(pkg.repository.url);
 
+const github = githubUrlFromGit(pkg.repository.url);
 if (!github) {
   console.error('Error: Must have GitHub repository in package.json');
   process.exit(1);
 }
+
+// TODO: Allow HISTORY.md, etc.
+const changelogPath = path.join(pkgPath, '../CHANGELOG.md');
+let changelogContents = '';
+try {
+  changelogContents = fs.readFileSync(changelogPath, 'utf8');
+} catch (err) {
+  if (err.code !== 'ENOENT') {
+    console.error(err);
+    process.exit(1);
+  }
+}
+
+const headingType = getHeadingType(changelogContents);
 
 const port = startServer({ handleData, json2markdown });
 
@@ -29,33 +44,46 @@ opn(url);
 
 function handleData(data) {
   console.log();
-  // TODO: Allow HISTORY.md, etc.
-  const changelogPath = path.join(pkgPath, '../CHANGELOG.md');
 
-  fs.readFile(changelogPath, 'utf8', (err, oldMarkdown = '') => {
-    if (err && err.code === 'ENOENT') {
-      console.log(`Creating new file: ${changelogPath}`);
-    } else if (err) return console.error(err);
-    else console.log(`Prepending to ${changelogPath}`);
+  if (!changelogContents) console.log(`Creating new file: ${changelogPath}`);
+  else console.log(`Prepending to ${changelogPath}`);
 
-    console.log();
-    console.log(ensureTrailingNewline(data));
+  console.log();
+  console.log(ensureTrailingNewline(data));
 
-    fs.writeFile(
-      changelogPath,
-      ensureTrailingNewline(data + oldMarkdown),
-      (err) => err && console.error(err)
-    );
-  });
+  fs.writeFile(
+    changelogPath,
+    ensureTrailingNewline(data + changelogContents),
+    (err) => err && console.error(err)
+  );
 }
 
 function json2markdown(data) {
   const { changes, version } = data;
+
+  const heading = textToHeading(`${version} / ${timestamp()}`, headingType);
+
   const list = !changes.length
     ? ''
-    : `- ${changes.map(changeToString).join('\n- ')}\n`;
+    : `- ${changes.map(changeToString).join('\n- ')}\n\n`;
 
-  return `# ${version} / ${timestamp()}\n\n${list}\n`;
+  return heading + list;
+}
+
+function getHeadingType(markdown) {
+  let headingType = { level: 1, style: 'normal' };
+  const lines = markdown.split('\n');
+  // match pound sign headings
+  const normalHeadingMatch = lines[0].match(
+    /(#{1,6}) \d+(\.\d+){2}.* \/ \d{4}(-\d{2}){2}/
+  );
+  if (normalHeadingMatch) headingType.level = normalHeadingMatch[1].length;
+  // match underline headings
+  if (lines[0].match(/\d+(\.\d+){2}.* \/ \d{4}(-\d{2}){2}/)) {
+    if (lines[1].indexOf('=') === 0) headingType.style = 'alt';
+    if (lines[1].indexOf('-') === 0) headingType = { level: 2, style: 'alt' };
+  }
+  return headingType;
 }
 
 function changeToString(change) {
